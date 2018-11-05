d6bb3bd1e2d3623b218b8fdfdd21fdf210a0ae93
William B <william@barkoffusa.com>
2018-11-05 13:31:26 -0500

    add settings screen (doesn't do much) and sign out button

--- Components/Views/SettingsView.js
+++ Components/Views/SettingsView.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import { AsyncStorage, Text } from 'react-native'
+import { AsyncStorage, Text, ScrollView, StyleSheet, Button } from 'react-native'
 import { createStackNavigator } from 'react-navigation';
 
 import Loading from '../Loading.js'
 
 class SettingsView extends React.Component {
     static navigationOptions = {
-		title: "Settings",
-	};
+        title: "Settings",
+    };
 
     constructor(props) {
         super(props)
@@ -19,40 +19,62 @@ class SettingsView extends React.Component {
 
     load() {
         AsyncStorage.getItem('csrfToken').then((token) => {
-			fetch('https://api-v2.myhomework.space/auth/me?csrfToken=' + token, {
-				method: 'GET',
-				credentials: 'include'
-			}).then((response) => {
-				return response.text();
-			}).then((text) => {
+            fetch('https://api-v2.myhomework.space/auth/me?csrfToken=' + token, {
+                method: 'GET',
+                credentials: 'include'
+            }).then((response) => {
+                return response.text();
+            }).then((text) => {
                 console.log(text);
-				this.setState({
+                this.setState({
                     me: JSON.parse(text),
                     loading: false,
                 });
                 console.log(this.state.me)
-			}).catch((error) => {
-				console.error(error)
-			});
-		}).catch((error) => {
-			console.error(error);
-		})
+            }).catch((error) => {
+                console.error(error)
+            });
+        }).catch((error) => {
+            console.error(error);
+        })
     }
 
     render() {
-        if(!this.state.loading) {
-            return (<Text>
-                {JSON.stringify(this.state.me)}
-            </Text>)
+        if (!this.state.loading) {
+            return (<ScrollView>
+                <Text style={styles.hello}>Hi, {this.state.me.user.name.split(' ')[0]}</Text>
+                <Text style={styles.subtitle}>Grade {this.state.me.grade} | {this.state.me.user.email} {this.state.me.level > 0 ? (<Text>| <Text style={styles.admin}>Admin</Text></Text>) : null}</Text>
+                <Button title="Sign Out" color="red" onPress={async () => {
+                    await AsyncStorage.clear();
+                    this.props.navigation.navigate('Auth');
+                }} />
+            </ScrollView>)
         } else return <Loading />
     }
 }
 
+const styles = StyleSheet.create({
+    hello: {
+        paddingTop: 10,
+        fontWeight: "bold",
+        fontSize: 30,
+        textAlign: "center"
+    },
+    subtitle: {
+        textAlign: "center",
+        color: "#777777",
+        paddingTop: 5
+    },
+    admin: {
+        color: "blue"
+    }
+})
+
 export default createStackNavigator(
-	{
-		SettingsView: SettingsView,
-	},
-	{
-		initialRouteName: 'SettingsView',
-	}
+    {
+        SettingsView: SettingsView,
+    },
+    {
+        initialRouteName: 'SettingsView',
+    }
 );
